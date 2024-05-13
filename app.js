e4178df740961b9b09dcfb9024f57808cfbf846e
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-05-13 14:19:00 +0530

    Added 2 problems

---
+++ app.js
@@ -0,0 +1,9 @@
+//    Q1:    Create a simple Node.js application that prints 'Hello, World!' to the console.
+//    ans:   console.log("Hello World")  //    JUST RUN THE COMMAND "NODE FILE NAME" HERE IT IS NODE app.js
+
+
+//    Q2: Create a Node.js application that takes two numbers as command-line arguments and prints their sum.
+//    ans:const num2 = parseFloat(process.argv[3]);
+//    ans:const num1 = parseFloat(process.argv[2]);
+//    ans:const num3 = parseFloat(process.argv[4])
+//    ans:console.log("Sum:", num1 + num2 + num3);
